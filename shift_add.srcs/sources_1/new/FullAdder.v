8a78a7430edc5acae7f7cc449725a467514c65dc
Yağız Mart <69057041+yazmrt@users.noreply.github.com>
2023-09-16 09:06:54 +0300

    Update FullAdder.v

--- shift_add.srcs/sources_1/new/FullAdder.v
+++ shift_add.srcs/sources_1/new/FullAdder.v
@@ -24,15 +24,10 @@ module FullAdder(
 input a_i,
 input b_i,
 input cin_i,
-output reg sum_o,
+output sum_o,
 output cout_o);
 
-//reg sum_reg;
-reg cout_reg;
 
-always @(*) begin
-    sum_o = a_i ^ b_i ^ cin_i;
-    cout_reg = (a_i & b_i) | (b_i & cin_i) | (a_i & cin_i);
-end
-assign cout_o = cout_reg;
+assign {cout_o,sum_o} = a_i+b_i+cin_i;
+
 endmodule
